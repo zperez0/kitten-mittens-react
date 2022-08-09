4c0ba3f0d1089f6b4d79a068c6f4fce206086952
Yazmin Perez <zoeperez333@gmail.com>
2022-08-08 20:52:54 -0700

    [WIP] add Header & Merch tag

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import Merch from "./Merch";
+
+function App() {
+  return (
+  <React.Fragment>
+    <Header />
+    <Merch />
+  </React.Fragment>
+  );
+}
+
+export default App;
